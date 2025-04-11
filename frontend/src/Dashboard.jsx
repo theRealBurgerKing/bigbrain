78dfc2caf22e1e0b02154dd58159eeb1d1dddb5b
Sheng, Zhetao <z5574315@ad.unsw.edu.au>
2025-04-11 16:06:41 +1000

    test：branch test

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,6 +1,10 @@
 function Dashboard(){
+  const token = localStorage.getItem('token')
+
   return (
-    <>Dashboard</>
+    <>Dashboard
+    {token ? <p>Token: {token}</p> : <p>No token found</p>}
+    </>
   )
 }
 export default Dashboard;
